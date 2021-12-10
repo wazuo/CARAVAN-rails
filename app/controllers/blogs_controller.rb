1f71d257345046d76ab7e87271e1f402b3943fad
EC2 Default User <95359918+wazuo@users.noreply.github.com>
2021-12-10 23:27:14 +0000

    [finish]3-7 問題解決 index.html.erbに問題ありclss名blog-text-hrading blogs_path(blog)部分単数形に変更

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -23,9 +23,13 @@ class BlogsController < ApplicationController
   def update
     blog = Blog.find(params[:id])
     blog.update(blog_params)
-    redirect_to blog_path(blog.id)
-  end  
-  
+    redirect_to blog_path(blog)
+  end
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
+  end
   
 private
   def blog_params
