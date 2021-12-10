7bc76ba0f51ac47b88b40bd0dc6c7c7dc8c4e814
EC2 Default User <95359918+wazuo@users.noreply.github.com>
2021-12-10 09:23:51 +0000

    [finidh]3-4

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,12 +1,13 @@
 class BlogsController < ApplicationController
   def index
+    @blogs =Blog.all
   end
 
   def show
   end
 
   def new
-    @blog= Blog.new
+    @blog = Blog.new
   end
   
   def create
